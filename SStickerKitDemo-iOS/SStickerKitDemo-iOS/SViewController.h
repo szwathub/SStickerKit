287dadf7aca1761868b5c610d1628ffb0bac0c50
Cyrex <szwathub@gmail.com>
2019-11-03 23:18:27 +0800

    Initial commit

---
+++ SStickerKitDemo-iOS/SStickerKitDemo-iOS/SViewController.h
@@ -0,0 +1,18 @@
+///
+///  Copyright © 2019 zhiweisun. All rights reserved.
+///
+///  File name: SViewController.h
+///  Author:    zhiweisun @Cyrex
+///  E-mail:    szwathub@gmail.com
+///
+///  Description:
+///
+///  History:
+///      11/03/2019: Created by Cyrex on 11/03/2019
+///
+
+#import <UIKit/UIKit.h>
+
+@interface SViewController : UIViewController
+
+@end
---
+++ SStickerKitDemo-iOS/SStickerKitDemo-iOS/SViewController.m
@@ -0,0 +1,21 @@
+///
+///  Copyright © 2019 zhiweisun. All rights reserved.
+///
+///  File name: SViewController.m
+///  Author:    zhiweisun @Cyrex
+///  E-mail:    szwathub@gmail.com
+///
+
+#import "SViewController.h"
+
+@interface SViewController ()
+
+@end
+
+@implementation SViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	/// Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
