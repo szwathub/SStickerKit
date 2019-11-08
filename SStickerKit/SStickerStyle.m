170743a3a972c1caefc81ccbe71522b7940008ab
Cyrex <szwathub@gmail.com>
2019-11-08 17:38:12 +0800

    finish version 1.0.0.

---
+++ SStickerKit/SStickerStyle.m
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2019 ZhiweiSun. All rights reserved.
+//
+//  File name: SStickerStyle.m
+//  Author:    Zhiwei Sun @Cyrex
+//  E-mail:    szwathub@gmail.com
+//
+//  Description:
+//
+//  History:
+//      2019/11/5: Created by Cyrex on 2019/11/5
+//
+
+#import "SStickerStyle.h"
+
+@implementation SStickerStyle
+
+@end
